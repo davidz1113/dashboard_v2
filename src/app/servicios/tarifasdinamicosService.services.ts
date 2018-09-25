38927bcaf78c5d2955e8023d91ad645cba4c6f30
Kevin Montufar <kmb_9408@hotmail.com>
2018-09-25 17:47:24 -0500

    Se corrije el componente de tarifa puesto eventual y se agrega el servicio de editar a TarifasService

--- src/app/servicios/tarifasdinamicosService.services.ts
+++ src/app/servicios/tarifasdinamicosService.services.ts
@@ -1,12 +1,7 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Http, Response, Headers, ResponseContentType } from '@angular/http';
-import { GLOBAL } from "./globales";
+import { GLOBAL } from './globales';
 import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { Subject } from "rxjs/Subject";
-import { Observable } from "rxjs/Observable";
-import { BehaviorSubject } from "rxjs/BehaviorSubject";
-import * as Rx from "rxjs";
 /**
  * Clase dinamica para generar consultas de las tablas de tarifas
  */
@@ -17,28 +12,28 @@ export class TarifasServices {
     public headers;
 
     constructor(private _http: Http) {
-        //this.url = 'http://192.168.1.21/SistemaRecaudoBackend/web/app_dev.php';
+        // this.url = 'http://192.168.1.21/SistemaRecaudoBackend/web/app_dev.php';
         this.url = GLOBAL.url;
         this.headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
     }
 
     /**
-     * 
+     *
      * @param url url del controlador donde se quiere hacer la peticion(llega del _router)
-     * Metodo que consulta las cabeceras de la consulta de tarifas asi como los datos para mostrar en su respectiva columna 
+     * Metodo que consulta las cabeceras de la consulta de tarifas asi como los datos para mostrar en su respectiva columna
      */
     consultarDatos(url: string) {
-        let params = "authorization=" + this.getToken();
+        const params = 'authorization=' + this.getToken();
         return this._http.post(this.url + url + '/query', params, { headers: this.headers })
             .pipe(map(res => res.json()));
     }
 
 
     /**
-     * Para crea cuqluier tarifa 
-     * @param pTarifa 
-     * @param uploadData 
-     * @param url 
+     * Para crea cuqluier tarifa
+     * @param pTarifa Tarifa a agregar
+     * @param uploadData Datos a subir
+     * @param url Url del servicio
      */
     crearTarifa(pTarifa: any, uploadData: FormData, url: string) {
         const json = JSON.stringify(pTarifa);
@@ -55,11 +50,32 @@ export class TarifasServices {
             );
     }
 
+    /**
+     * Edita cualquier tarifa
+     * @param pTarifa Tarifa a editar
+     * @param uploadData Datos a subir
+     * @param url Url del servicio
+     */
+    editarTarifa(pTarifa: any, uploadData: FormData, url: string) {
+        const json = JSON.stringify(pTarifa);
+        uploadData.append('json', json);
+        uploadData.append('authorization', this.token);
+        return this._http.post(this.url + url + '/edit', uploadData)
+            .pipe(
+                map(
+                    resp => {
+                        // console.log(resp)
+                        return resp.json();
+                    }
+                )
+            );
+    }
+
 
     getToken() {
-        let token = JSON.parse(localStorage.getItem('token'));
+        const token = JSON.parse(localStorage.getItem('token'));
 
-        if (token != "undefined") {
+        if (token !== 'undefined') {
             this.token = token;
         } else {
             this.token = null;
