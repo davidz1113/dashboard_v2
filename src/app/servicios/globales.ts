b5b5d4d55aaf30ea3f30f771ea4b67c1d12f917b
David Andres <d.carvajal_13@hotmail.com>
2018-09-28 19:00:07 -0500

    uniendo archivos kevin david

--- src/app/servicios/globales.ts
+++ src/app/servicios/globales.ts
@@ -2,7 +2,7 @@ export var GLOBAL = {
     //url: 'http://localhost/SistemaRecaudoBackend/web/app_dev.php',
     //url: 'http://192.168.1.21/SistemaRecaudoBackend/web/app_dev.php',
      url: 'http://contalentosas.com/SistemaRecaudoBackend/web/app_dev.php',
-     urlBase: 'SistemaRecaudd',
+     urlBase: 'SistemaRecaudo',
      urlImagen: 'http://contalentosas.com/SistemaRecaudoBackend/',
     
 }
